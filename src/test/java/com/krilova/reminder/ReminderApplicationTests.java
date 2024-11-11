c00c09851e919bd6a9912a1ec802146578fd14b9
korall79 <koralll79@yandex.ru>
2024-11-11 22:31:38 +0400

    start remainder

---
+++ src/test/java/com/krilova/reminder/ReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krilova.reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
